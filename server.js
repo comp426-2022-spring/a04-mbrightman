2427b4f1769d75c7a32942d17af3a2e01ee171be
mbrightman <brightman.matthew@gmail.com>
2022-04-19 13:28:41 -0400

    improved server.js code and attempting to fix error

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const fs = require('fs');
 
 const args = require('minimist')(process.argv.slice(2))
 
-var port = args.port || 5555
+const port = args.port || process.env.PORT || 5555
+
+const do_debug = args.debug || false
 
 const help = (`
 server.js [options]
@@ -43,88 +45,13 @@ const server = app.listen(port, () => {
     console.log("App is running on port %PORT%".replace("%PORT%", port))
 })
 
-/*
-    COIN FLIP FUNCTIONS
-*/
 
-function coinFlip() {
-    return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
-}
-
-function flipACoin(call) {
-    var flip = (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
-    if (flip == call) {
-        return {
-        'call': call,
-        'flip': flip,
-        'result': 'win'
-        }
-    } else {
-        return {
-        'call': call,
-        'flip': flip,
-        'result': 'lose'
-        }
-    }
-}
-
-function coinFlips(flips) {
-    var a = []
-    for (let i = 0; i < flips; i++) {
-      a[i] = (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
-    }
-    return a;
-}
-  
-  /** Count multiple flips
-   * 
-   * Write a function that accepts an array consisting of "heads" or "tails" 
-   * (e.g. the results of your `coinFlips()` function) and counts each, returning 
-   * an object containing the number of each.
-   * 
-   * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
-   * { tails: 5, heads: 5 }
-   * 
-   * @param {string[]} array 
-   * @returns {{ heads: number, tails: number }}
-   */
-  
-function countFlips(array) {
-    var tCounter = 0
-    var hCounter = 0
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] == 'heads') {
-        hCounter++
-      } else {
-        tCounter++
-      }
-    }
-  
-    if (tCounter === 0) {
-      return {
-        'heads': hCounter
-      }
-    } else if (hCounter === 0) {
-      return {
-        'tails': tCounter
-      }
-    } else {
-      return {
-        'tails': tCounter,
-        'heads': hCounter
-      }
-    }
-}
-
-/*
-    END FLIP FUNCS
-*/
 
 // default endpoint
 app.get('/app/', (req,res,next) => {
-  const statusCode = 200
-  const statusMessage = 'OK'
-  res.status(statusCode).end(statusCode + ' ' + statusMessage)
+  res.status(200)
+  res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' })
+  res.end(res.statusCode + ' ' + res.statusMessage)  
 })
 
 // app.use(logging('common', { stream: accessLog }))
@@ -176,28 +103,23 @@ app.get('/app/flip/call/tails/', (req, res, next) => {
     res.status(200).json(flip)
 })
 
-// adding /app/log/access and /app/error for if do_debug is true
-app.get('/app/log/access', (req, res, next) => {
-  if (args.debug === 'true') {
-    try{
+if (do_debug === true) {
+  app.get('/app/log/access', (req, res, next) => {
+    try {
       var stmt = logdb.prepare('SELECT * FROM accesslog').all();
       res.status(200).json(stmt)
-    } catch {
+    } catch (e) {
       console.error(e)
     }
-    
-  } else {
-    res.status(404).type('text/plain').send('404 NOT FOUND')
-  }
-})
-
-app.get('/app/error', (req, res, next) => {
-  if (args.debug === 'true') {
+  })
+  
+  app.get('/app/error', (req, res, next) => {
     throw new Error('Error test successful.')
-  } else {
-    res.status(404).type('text/plain').send('404 NOT FOUND')
-  }
-})
+  })
+}
+
+// adding /app/log/access and /app/error for if do_debug is true
+
 
 app.use(function(req, res, next) {
     // send turns text into html
@@ -206,4 +128,81 @@ app.use(function(req, res, next) {
     res.type("text/plain")
 })
 
-// adding comment to push and try again.
+
+
+/*
+    COIN FLIP FUNCTIONS
+*/
+
+function coinFlip() {
+  return (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+}
+
+function flipACoin(call) {
+  var flip = (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+  if (flip == call) {
+      return {
+      'call': call,
+      'flip': flip,
+      'result': 'win'
+      }
+  } else {
+      return {
+      'call': call,
+      'flip': flip,
+      'result': 'lose'
+      }
+  }
+}
+
+function coinFlips(flips) {
+  var a = []
+  for (let i = 0; i < flips; i++) {
+    a[i] = (Math.floor(Math.random() * 2) == 0) ? 'heads' : 'tails';
+  }
+  return a;
+}
+
+/** Count multiple flips
+ * 
+ * Write a function that accepts an array consisting of "heads" or "tails" 
+ * (e.g. the results of your `coinFlips()` function) and counts each, returning 
+ * an object containing the number of each.
+ * 
+ * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
+ * { tails: 5, heads: 5 }
+ * 
+ * @param {string[]} array 
+ * @returns {{ heads: number, tails: number }}
+ */
+
+function countFlips(array) {
+  var tCounter = 0
+  var hCounter = 0
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == 'heads') {
+      hCounter++
+    } else {
+      tCounter++
+    }
+  }
+
+  if (tCounter === 0) {
+    return {
+      'heads': hCounter
+    }
+  } else if (hCounter === 0) {
+    return {
+      'tails': tCounter
+    }
+  } else {
+    return {
+      'tails': tCounter,
+      'heads': hCounter
+    }
+  }
+}
+
+/*
+  END FLIP FUNCS
+*/
