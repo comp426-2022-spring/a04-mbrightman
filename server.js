d6e1bab949842c57ae9f036fa654cf2b9263c6dc
mbrightman <brightman.matthew@gmail.com>
2022-04-19 13:10:35 -0400

    updated server functions

--- server.js
+++ server.js
@@ -14,23 +14,26 @@ var args = require('minimist')(process.argv.slice(2))
 
 const port = args.port || process.env.PORT || 5555
 
+const help = (`
+server.js [options]
+
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+
+--help	Return this message and exit.
+`)
+
 if (args.help || args.h) {
-    console.log(`server.js [options]
-
-    --port		Set the port number for the server to listen on. Must be an integer
-                  between 1 and 65535.
-  
-    --debug	If set to true, creates endlpoints /app/log/access/ which returns
-                  a JSON access log from the database and /app/error which throws 
-                  an error with the message "Error test successful." Defaults to 
-      false.
-  
-    --log		If set to false, no log files are written. Defaults to true.
-      Logs are always written to database.
-  
-    --help	Return this message and exit.`)
-
-    process.exit(1)
+    console.log(help)
+    process.exit(0)
 }
 
 const server = app.listen(port, () => {
@@ -128,6 +131,8 @@ app.use( (req, res, next) => {
     referer: req.headers['referer'],
     useragent: req.headers['user-agent']
   }
+
+  console.log(logdata)
 
   const stmt = logdb.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
   const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
